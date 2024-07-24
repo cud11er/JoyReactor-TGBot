9947b21c5cec5a1bfec149be4094f977860419df
cud11er <rikunov.kost@inbox.ru>
2024-07-24 12:52:59 +0300

    Добавлен механизм с выводом картинок последующих страниц сайта, добавлена кнопка в бота. (Попытка добавить парсинг видео - неудачно)

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import telebot
-from telebot import types
 from dotenv import load_dotenv
 import os
-from get_image import save_image_from_url, fetch_images_from_joyreactor, clean_filename, load_cookies
+from get_image import save_media_from_url, fetch_media_from_joyreactor, clean_filename, load_cookies
 import logging
 from concurrent.futures import ThreadPoolExecutor
 
@@ -27,49 +26,51 @@ user_tags = []
 current_page = 1
 
 # Загрузка куки из файла
-cookies = load_cookies('cookies.json')
+cookies = load_cookies('cookies.json')  # Заменил на новое имя файла
 
-# Функция для получения и отправки изображений
-def send_images(search_query, tags, chat_id, page=1):
+# Функция для получения и отправки медиа
+def send_media(search_query, tags, chat_id, page):
     try:
-        # Вызываем функцию для парсинга и получения изображений
-        images = fetch_images_from_joyreactor(search_query, tags, cookies, page)
+        media = fetch_media_from_joyreactor(search_query, tags, cookies, page)
 
-        if not images:
-            bot.send_message(chat_id, "Изображения не найдены.")
+        if not media:
+            bot.send_message(chat_id, "Медиа не найдено.")
             return
 
-        for image in images:
-            image_url = image['imageUrl']
-            file_name = clean_filename(image_url.split("/")[-1])
+        for item in media:
+            media_url = item['url']
+            file_name = clean_filename(media_url.split("/")[-1])
             file_path = os.path.join(os.getcwd(), file_name)
-            save_image_from_url(image_url, file_path)  # Сохраняем изображение
+            save_media_from_url(media_url, file_path)  # Сохраняем медиа
 
             # Проверяем размер файла и отправляем его соответствующим образом
             file_size = os.path.getsize(file_path)
             with open(file_path, 'rb') as file:
-                if file_size > 10 * 1024 * 1024:  # 10 MB
-                    bot.send_document(chat_id, file)
-                else:
-                    bot.send_photo(chat_id, file)
+                if item['type'] == 'image':
+                    if file_size > 10 * 1024 * 1024:  # 10 MB
+                        bot.send_document(chat_id, file)
+                    else:
+                        bot.send_photo(chat_id, file)
+                elif item['type'] == 'video':
+                    bot.send_video(chat_id, file)
 
-            os.remove(file_path)  # Удаляем изображение после отправки
-            logging.info(f"Изображение удалено: {file_path}")
+            os.remove(file_path)  # Удаляем медиа после отправки
+            logging.info(f"Медиа удалено: {file_path}")
 
-        # Кнопки для управления страницами
-        markup = types.InlineKeyboardMarkup()
-        next_page_button = types.InlineKeyboardButton('Следующая страница', callback_data=f'next_page:{page+1}')
-        markup.add(next_page_button)
-        bot.send_message(chat_id, "Загрузить больше изображений?", reply_markup=markup)
+        # Отправка кнопки "Следующая страница"
+        markup = telebot.types.InlineKeyboardMarkup()
+        next_page_btn = telebot.types.InlineKeyboardButton("Следующая страница", callback_data=f"next_page_{page + 1}")
+        markup.add(next_page_btn)
+        bot.send_message(chat_id, "Все медиа отправлены! Хотите посмотреть больше?", reply_markup=markup)
 
     except Exception as e:
         bot.send_message(chat_id, f"Произошла ошибка: {e}")
-        logging.error(f"Error in send_images: {e}")
+        logging.error(f"Error in send_media: {e}")
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, ("Привет! Отправь мне ключевое слово для поиска изображений (или оставь пустым) "
+    bot.reply_to(message, ("Привет! Отправь мне ключевое слово для поиска медиа (или оставь пустым) "
                            "и теги через запятую (или оставь пустыми). Формат: ключевое слово, тег1, тег2 и т.д.\n"
                            "Примеры:\n"
                            "'Собака улыбка (Если теги не нужны - просто ключевое слово)'\n"
@@ -87,26 +88,30 @@ def handle_message(message):
         user_tags = [tag.strip() for tag in parts[1:] if tag.strip()] if len(parts) > 1 else []
         current_page = 1
 
-        bot.reply_to(message, "Получил запрос. Ищу изображения...")
+        bot.reply_to(message, "Получил запрос. Ищу медиа...")
 
-        # Вызываем функцию отправки изображений асинхронно
-        executor.submit(send_images, user_search_query, user_tags, message.chat.id, current_page)
+        # Вызываем функцию отправки медиа асинхронно
+        executor.submit(send_media, user_search_query, user_tags, message.chat.id, current_page)
 
     except Exception as e:
         bot.reply_to(message, f"Произошла ошибка при обработке запроса: {e}")
         logging.error(f"Error in handle_message: {e}")
 
-# Обработчик нажатий на inline-кнопки
-@bot.callback_query_handler(func=lambda call: call.data.startswith('next_page:'))
-def handle_pagination(call):
-    global current_page
+# Обработчик callback данных для кнопок
+@bot.callback_query_handler(func=lambda call: call.data.startswith('next_page_'))
+def handle_next_page_callback(call):
+    global user_search_query, user_tags
+
     try:
-        current_page = int(call.data.split(':')[1])
-        bot.answer_callback_query(call.id, f"Загрузка страницы {current_page}...")
-        executor.submit(send_images, user_search_query, user_tags, call.message.chat.id, current_page)
+        next_page = int(call.data.split('_')[-1])
+        bot.send_message(call.message.chat.id, f"Ищу медиа на странице {next_page}...")
+
+        # Вызываем функцию отправки медиа асинхронно
+        executor.submit(send_media, user_search_query, user_tags, call.message.chat.id, next_page)
+
     except Exception as e:
-        bot.send_message(call.message.chat.id, f"Произошла ошибка при загрузке страницы: {e}")
-        logging.error(f"Error in handle_pagination: {e}")
+        bot.send_message(call.message.chat.id, f"Произошла ошибка при обработке запроса: {e}")
+        logging.error(f"Error in handle_next_page_callback: {e}")
 
 # Запуск бота
 bot.infinity_polling()
--- get_image.py
+++ get_image.py
@@ -5,7 +5,6 @@ from urllib.parse import urljoin, urlencode, unquote
 import re
 import json
 
-# Загружаем куки из файла
 def load_cookies(cookie_file):
     with open(cookie_file, 'r') as f:
         cookies = json.load(f)
@@ -14,7 +13,6 @@ def load_cookies(cookie_file):
         cookie_dict[cookie['name']] = cookie['value']
     return cookie_dict
 
-# Функция для получения HTML кода страницы по заданному URL с использованием куки
 def get_html(url, cookies):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
@@ -26,55 +24,52 @@ def get_html(url, cookies):
         print(f'Error {response.status_code} occurred.')
         return None
 
-# Функция для сохранения изображения
-def save_image_from_url(img_url, file_path):
+def save_media_from_url(media_url, file_path):
     try:
-        # Скачиваем и сохраняем изображение
-        with requests.get(img_url, stream=True) as r:
-            r.raise_for_status()
+        response = requests.get(media_url, stream=True)
+        if response.status_code == 200:
             with open(file_path, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8192):
+                for chunk in response.iter_content(chunk_size=8192):
                     f.write(chunk)
-        print(f'Saved image: {file_path}')
+            if os.path.exists(file_path):
+                print(f'Successfully saved media: {file_path}')
+            else:
+                print(f'Error: File not saved correctly: {file_path}')
+        else:
+            print(f'Error {response.status_code} fetching media: {media_url}')
     except Exception as e:
-        print(f'Error saving image {img_url}: {e}')
+        print(f'Error saving media {media_url}: {e}')
 
-# Функция для очистки имени файла
 def clean_filename(filename):
-    # Декодируем URL, чтобы получить нормальный текст
     filename = unquote(filename)
-    # Удаляем недопустимые символы
     filename = re.sub(r'[\\/*?:"<>|]', "", filename)
     return filename
 
-# Функция для парсинга страницы и извлечения ссылок на изображения
-def fetch_images_from_joyreactor(search_query, tags, cookies, page=1):
+def fetch_media_from_joyreactor(search_query, tags, cookies, page=1):
     try:
-        # Формируем базовый URL joyreactor для поиска
-        if page == 1:
-            base_url = 'https://joyreactor.cc/search?'
+        base_url = 'https://joyreactor.cc/search?'
+        params = {
+            'q': search_query,
+            'tags': ', '.join(tags)
+        }
+
+        if page > 1:
             params = {
-                'q': search_query,
                 'tags': ', '.join(tags)
             }
-            full_url = base_url + urlencode(params, safe=',')
+            full_url = f"https://joyreactor.cc/search/{search_query}/{page}?" + urlencode(params, safe=',')
         else:
-            base_url = f'https://joyreactor.cc/search/{search_query}/{page}?'
-            params = {
-                'tags': ', '.join(tags)
-            }
             full_url = base_url + urlencode(params, safe=',')
 
         print(f'Search URL: {full_url}')
 
-        # Получаем HTML страницы с результатами поиска
         html = get_html(full_url, cookies)
         if html:
             soup = BeautifulSoup(html, 'html.parser')
             post_list = soup.find('div', id='post_list')
             if post_list:
                 articles = post_list.find_all('div', class_='article post-normal')
-                images = []
+                media = []
                 for article in articles:
                     post_content = article.find('div', class_='post_content')
                     if post_content:
@@ -83,8 +78,19 @@ def fetch_images_from_joyreactor(search_query, tags, cookies, page=1):
                             img_tag = image_container.find('img')
                             if img_tag and img_tag.has_attr('src'):
                                 img_url = urljoin('https://joyreactor.cc/', img_tag['src'])
-                                images.append({'imageUrl': img_url})
-                return images
+                                media.append({'type': 'image', 'url': img_url})
+
+                            #video_tag = image_container.find('video')
+                            #if video_tag:
+                            #    sources = video_tag.find_all('source')
+                            #    for source in sources:
+                            #        if source.has_attr('src'):
+                            #            video_url = urljoin('https://joyreactor.cc/', source['src'])
+                            #            # Печать для проверки
+                            #            print(f'Found video URL: {video_url}')
+                            #            media.append({'type': 'video', 'url': video_url})
+#
+                return media
             else:
                 print('No post list found.')
                 return None
@@ -92,24 +98,5 @@ def fetch_images_from_joyreactor(search_query, tags, cookies, page=1):
             print('HTML content not retrieved.')
             return None
     except Exception as e:
-        print(f'Error fetching images: {e}')
+        print(f'Error fetching media: {e}')
         return None
-
-
-# Основная функция для выполнения скрипта
-if __name__ == "__main__":
-    search_query = input("Enter search query: ")
-    tags = input("Enter tags (comma separated): ").split(',')
-    tags = [tag.strip() for tag in tags]
-
-    cookies = load_cookies('cookie.json')
-    images = fetch_images_from_joyreactor(search_query, tags, cookies)
-
-    if images:
-        for image in images:
-            image_url = image['imageUrl']
-            file_name = clean_filename(image_url.split("/")[-1])
-            file_path = os.path.join(os.getcwd(), file_name)
-            save_image_from_url(image_url, file_path)
-    else:
-        print("No images found.")
